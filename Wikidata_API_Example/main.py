1ee2e887eac8a94a6f7efada0f11f4d12d6eceaf
seckum <48880715+seckum@users.noreply.github.com>
2020-11-10 01:13:34 +0300

    Wikidata API usage example added

---
+++ Wikidata_API_Example/main.py
@@ -0,0 +1,26 @@
+import requests
+
+url = "https://www.wikidata.org/w/api.php"
+
+
+while True:
+    query = input("Enter name : ")
+    if query == "quit":
+        break
+    else:
+        params = {
+        "action" : "wbsearchentities",
+        "language" : "tr",
+        "format" : "json",
+        "search" : query
+        }
+        try:
+            data = requests.get(url,params=params)
+            print(data.json()["search"][0]["description"])
+        except:
+            print("Invalid Input try again !!!")
+
+
+
+
+
